3a32c82826fb9cc1db651ca2745b2166ccc762f6
Caden McVey <59580833+caden602@users.noreply.github.com>
2024-04-03 19:21:45 -0600

    Add dearpygui

--- DearPyGUI/GUI.py
+++ DearPyGUI/GUI.py
@@ -1,22 +1,72 @@
 import dearpygui.dearpygui as dpg
 
-dpg.create_context() # First DPG Command
+# Define an empty dictionary to hold enum-like values
+row_dict = {}
+col_dict = {}
 
-with dpg.window(label="LunaSat"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+# TEMP
+col_labels = "col1 col2 col3" #temporary col element names
+row_labels = "row1 row2 row3 row4" #temporary row element names
 
-dpg.create_viewport(title='Custom Title', width=600, height=200)
+dpg.create_context()
+
+# # Add labes for each column
+# def table_column_labels(labels):
+#     # Split the input string into individual tokens
+#     tokens = labels.split()
+
+#     for token in tokens:
+#         if token.upper() not in col_dict:
+#             col_dict[token.upper()] = token.upper()
+
+#     list = [col_dict[token.upper()] for token in tokens]
+
+#     for i in range(len(list)):
+#         dpg.add_table_column(label=list[i])
+
+#     return len(list)
+
+# Add labels for each row
+def table_row_labels(labels):
+    # Split the input string into individual tokens
+    tokens = labels.split()
+
+    for token in tokens:
+        if token.upper() not in row_dict:
+            row_dict[token.upper()] = token.upper()
+
+    list = [row_dict[token.upper()] for token in tokens]
+    # for i in range(len(list)):
+    #     with dpg.table_row():
+    #         dpg.add_text(list[i])
+
+    return list, len(list)
+
+###########################################################
+with dpg.window(label="Display Data"):
+    with dpg.table(header_row=True):
+        dpg.add_table_column() # Column for row labels
+    
+        # cols_len = table_column_labels(col_labels)
+
+        dpg.add_table_column(label="Accelerometer")
+        dpg.add_table_column(label="Magnetometer")
+        dpg.add_table_column(label="Pressure Sensor")
+        dpg.add_table_column(label="Temp Sensor")
+        dpg.add_table_column(label="Gas Sensor")
+
+        list, rows_len = table_row_labels(row_labels)
+        print(rows_len)
+
+        for i in range(rows_len): # Rows
+            with dpg.table_row():
+                for j in range(0,6): # Columns
+                    with dpg.table_cell():
+                        dpg.add_text("data")
+        
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-
-# below replaces, start_dearpygui()
-while dpg.is_dearpygui_running():
-    # insert here any code you would like to run in the render loop
-    # you can manually stop by using stop_dearpygui()
-    print("this will run every frame")
-    dpg.render_dearpygui_frame()
-
-dpg.destroy_context() # Cleanup
+dpg.start_dearpygui()
+dpg.destroy_context()
