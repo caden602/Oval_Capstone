56ca8619edf0741b4c2f7982f7a542ea5dfe43ee
Caden McVey <59580833+caden602@users.noreply.github.com>
2024-04-04 02:55:28 -0600

    Working Data Table

--- DearPyGUI/GUI.py
+++ DearPyGUI/GUI.py
@@ -1,6 +1,11 @@
 import dearpygui.dearpygui as dpg
 import serial.tools.list_ports
 import serial
+import datetime
+import time
+
+# Global Variables
+choice = 1
 
 def detect_serial_port():
     # Get a list of all available serial ports
@@ -37,94 +42,109 @@ serial_port = "/dev/cu.usbmodem141301"
 # Initialize the serial connection
 ser = serial.Serial(serial_port, baud_rate)
 
-# Global defs
-temp_data = None
-
-try:
-    with serial.Serial(serial_port, baud_rate) as ser:
-        with open(output_file_path, 'w') as out_file:
-            while True:
-                line = ser.readline().decode('utf-8').rstrip()  # Read a line and strip newline
-                print(line)  # Optional: echo to console
-
-                # Extract the first data point
-                data_points = line.split(',')  # Split the line by commas
-                temp_data = data_points[0]  # Get the first element
-
-                out_file.write(line + '\n')  # Write to the file
-                
-except KeyboardInterrupt:
-    # Close the serial connection when Ctrl+C is pressed
-    ser.close()
-
-# Define an empty dictionary to hold enum-like values
-row_dict = {}
-col_dict = {}
-
-# TEMP
-col_labels = "col1 col2 col3" #temporary col element names
-row_labels = "row1 row2 row3 row4" #temporary row element names
+###########################################################
+# Define a callback function for when a tab is clicked
+def tab_callback(sender, app_data):
+    print("Tab", app_data, "clicked")
 
 dpg.create_context()
-
-# # Add labes for each column
-# def table_column_labels(labels):
-#     # Split the input string into individual tokens
-#     tokens = labels.split()
-
-#     for token in tokens:
-#         if token.upper() not in col_dict:
-#             col_dict[token.upper()] = token.upper()
-
-#     list = [col_dict[token.upper()] for token in tokens]
-
-#     for i in range(len(list)):
-#         dpg.add_table_column(label=list[i])
-
-#     return len(list)
-
-# Add labels for each row
-def table_row_labels(labels):
-    # Split the input string into individual tokens
-    tokens = labels.split()
-
-    for token in tokens:
-        if token.upper() not in row_dict:
-            row_dict[token.upper()] = token.upper()
-
-    list = [row_dict[token.upper()] for token in tokens]
-    # for i in range(len(list)):
-    #     with dpg.table_row():
-    #         dpg.add_text(list[i])
-
-    return list, len(list)
-
-###########################################################
-with dpg.window(label="Display Data"):
-    with dpg.table(header_row=True):
-        dpg.add_table_column() # Column for row labels
-    
-        # cols_len = table_column_labels(col_labels)
-
-        dpg.add_table_column(label="Accelerometer")
-        dpg.add_table_column(label="Magnetometer")
-        dpg.add_table_column(label="Pressure Sensor")
-        dpg.add_table_column(label="Temp Sensor")
-        dpg.add_table_column(label="Gas Sensor")
-
-        list, rows_len = table_row_labels(row_labels)
-        print(rows_len)
-
-        for i in range(rows_len): # Rows
-            with dpg.table_row():
-                for j in range(0,6): # Columns
-                    with dpg.table_cell():
-                        dpg.add_text(temp_data)
-        
-
-dpg.create_viewport(title='Custom Title', width=800, height=600)
+dpg.create_viewport(title='LunaSat GUI', width=800, height=800)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.start_dearpygui()
+
+with serial.Serial(serial_port, baud_rate) as ser:
+    with open(output_file_path, 'w') as out_file:
+        if choice == 1:
+            with dpg.window(label="Graph", height=600, width=600):
+                while dpg.is_dearpygui_running():
+                    line = ser.readline().decode('utf-8').rstrip()  # Read a line and strip newline
+                    print(line)  # Optional: echo to console
+
+                    # Extract the first data point
+                    data_points = line.split(',')  # Split the line by commas
+                    # Parse sensor data
+                    temp_data = data_points[0]  # Get the first element
+                    humidity_data = data_points[1]
+                    gas_data = data_points[2]
+                    pressure_data = data_points[3]
+                    adxl_data_x = data_points[4]
+                    adxl_data_y = data_points[5]
+                    adxl_data_z = data_points[6]
+                    lis3md_data_x = data_points[7]
+                    lis3md_data_y = data_points[8]
+                    lis3md_data_z = data_points[9]
+
+                    out_file.write(line + '\n')  # Write to the file
+
+                    dpg.add_simple_plot(label="Histogram", default_value=(float(temp_data), float(humidity_data)), overlay="Overlaying", height=600, width=600,
+                        histogram=True)
+                    dpg.render_dearpygui_frame()    
+                    time.sleep(0.0001)
+
+        if choice == 2:
+            with dpg.window(label="Data Table"):
+                with dpg.table(header_row=True):
+                    dpg.add_table_column(label="Time")
+                    dpg.add_table_column(label="Temp Sensor")
+                    dpg.add_table_column(label="Humidity Sensor")
+                    dpg.add_table_column(label="Gas Sensor")
+                    dpg.add_table_column(label="Pressure Sensor")
+                    dpg.add_table_column(label="Accelerometer")
+                    dpg.add_table_column(label="Magnetometer")
+
+                    while dpg.is_dearpygui_running():
+                        line = ser.readline().decode('utf-8').rstrip()  # Read a line and strip newline
+                        print(line)  # Optional: echo to console
+
+                        # Extract the first data point
+                        data_points = line.split(',')  # Split the line by commas
+                        # Parse sensor data
+                        temp_data = data_points[0]  # Get the first element
+                        humidity_data = data_points[1]
+                        gas_data = data_points[2]
+                        pressure_data = data_points[3]
+                        adxl_data_x = data_points[4]
+                        adxl_data_y = data_points[5]
+                        adxl_data_z = data_points[6]
+                        lis3md_data_x = data_points[7]
+                        lis3md_data_y = data_points[8]
+                        lis3md_data_z = data_points[9]
+
+                        out_file.write(line + '\n')  # Write to the file
+
+                        with dpg.table_row():
+                            for i in range(0,7): # Columns
+                                with dpg.table_cell():
+                                    if i == 0:
+                                        # Get the current date and time
+                                        timestamp = datetime.datetime.now()
+                                        # Extract the time portion
+                                        current_time = timestamp.strftime("%H:%M:%S")
+                                        dpg.add_text(current_time)
+                                    elif i == 1:
+                                        dpg.add_text(temp_data)
+                                    elif i == 2:
+                                        dpg.add_text(humidity_data)
+                                    elif i == 3:
+                                        dpg.add_text(gas_data)
+                                    elif i == 4:
+                                        dpg.add_text(pressure_data)
+                                    elif i == 5:
+                                        dpg.add_text(adxl_data_x+' '+adxl_data_y+' '+adxl_data_z)
+                                    elif i == 6:
+                                        dpg.add_text(lis3md_data_x+' '+lis3md_data_y+' '+lis3md_data_z)
+                                    else:
+                                        dpg.add_text('NULL')
+                        
+        
+                        dpg.render_dearpygui_frame()    
+                        time.sleep(0.0001)
+            
+        
+
+# dpg.create_viewport(title='Custom Title', width=800, height=600)
+# dpg.setup_dearpygui()
+# dpg.show_viewport()
+# dpg.start_dearpygui()
 dpg.destroy_context()
 
