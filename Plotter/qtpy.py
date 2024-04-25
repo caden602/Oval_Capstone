5b534aac220a27cc8de8138eb53bbf0e6545ee3d
Caden McVey <59580833+caden602@users.noreply.github.com>
2024-04-24 18:47:53 -0600

    Add 4 plot functionality with new codebase

--- Plotter/qtpy.py
+++ Plotter/qtpy.py
@@ -1,89 +1,169 @@
-"""
-This example demonstrates many of the 2D plotting capabilities
-in pyqtgraph. All of the plots may be panned/scaled by dragging with 
-the left/right mouse buttons. Right click on any plot to show a context menu.
-"""
-
 import numpy as np
-
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
 import pyqtgraph.opengl as gl
+import serial_helper as sh
+import multiprocessing
+import pyqtgraph.examples
+import time
 
-app = pg.mkQApp("Plotting Example")
-#mw = QtWidgets.QMainWindow()
-#mw.resize(800,800)
-
-win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
-win.resize(1000,600)
-win.setWindowTitle('LunaSat Graphs')
-
-# Enable antialiasing for prettier plots
-pg.setConfigOptions(antialias=True)
-
-p1 = win.addPlot(title="Updating plot")
-curve = p1.plot(pen='y')
-data = np.random.normal(size=(10,1000))
-ptr = 0
-def update():
-    global curve, data, ptr, p1
-    curve.setData(data[ptr%10])
-    if ptr == 0:
-        p1.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
-    ptr += 1
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(50)
 
 
 ##################
 
-## Create a GL View widget to display data
-app2 = pg.mkQApp("GLSurfacePlot Example")
-w = gl.GLViewWidget()
-w.show()
-w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
-w.setCameraPosition(distance=50)
+# ## Create a GL View widget to display data
+# app2 = pg.mkQApp("GLSurfacePlot Example")
+# w = gl.GLViewWidget()
+# w.show()
+# w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
+# w.setCameraPosition(distance=50)
 
-## Add a grid to the view
-g = gl.GLGridItem()
-g.scale(2,2,1)
-g.setDepthValue(10)  # draw grid after surfaces since they may be translucent
-w.addItem(g)
+# ## Add a grid to the view
+# g = gl.GLGridItem()
+# g.scale(2,2,1)
+# g.setDepthValue(10)  # draw grid after surfaces since they may be translucent
+# w.addItem(g)
 
+# ## Animated example
+# ## compute surface vertex data
+# cols = 90
+# rows = 100
+# x = np.linspace(-8, 8, cols+1).reshape(cols+1,1)
+# y = np.linspace(-8, 8, rows+1).reshape(1,rows+1)
+# d = (x**2 + y**2) * 0.1
+# d2 = d ** 0.5 + 0.1
 
-
-## Animated example
-## compute surface vertex data
-cols = 90
-rows = 100
-x = np.linspace(-8, 8, cols+1).reshape(cols+1,1)
-y = np.linspace(-8, 8, rows+1).reshape(1,rows+1)
-d = (x**2 + y**2) * 0.1
-d2 = d ** 0.5 + 0.1
-
-## precompute height values for all frames
-phi = np.arange(0, np.pi*2, np.pi/20.)
-z = np.sin(d[np.newaxis,...] + phi.reshape(phi.shape[0], 1, 1)) / d2[np.newaxis,...]
+# ## precompute height values for all frames
+# phi = np.arange(0, np.pi*2, np.pi/20.)
+# z = np.sin(d[np.newaxis,...] + phi.reshape(phi.shape[0], 1, 1)) / d2[np.newaxis,...]
 
 
-## create a surface plot, tell it to use the 'heightColor' shader
-## since this does not require normal vectors to render (thus we 
-## can set computeNormals=False to save time when the mesh updates)
-p4 = gl.GLSurfacePlotItem(x=x[:,0], y = y[0,:], shader='heightColor', computeNormals=False, smooth=False)
-p4.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
-p4.translate(10, 10, 0)
-w.addItem(p4)
+# ## create a surface plot, tell it to use the 'heightColor' shader
+# ## since this does not require normal vectors to render (thus we 
+# ## can set computeNormals=False to save time when the mesh updates)
+# p4 = gl.GLSurfacePlotItem(x=x[:,0], y = y[0,:], shader='heightColor', computeNormals=False, smooth=False)
+# p4.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
+# p4.translate(10, 10, 0)
+# w.addItem(p4)
 
-index = 0
-def update2():
-    global p4, z, index
-    index -= 1
-    p4.setData(z=z[index%z.shape[0]])
+# index = 0
+# def update2():
+#     global p4, z, index
+#     index -= 1
+#     p4.setData(z=z[index%z.shape[0]])
     
-timer2 = QtCore.QTimer()
-timer2.timeout.connect(update2)
-timer2.start(30)
+# timer2 = QtCore.QTimer()
+# timer2.timeout.connect(update2)
+# timer2.start(30)
 
-if __name__ == '__main__':
+class SatelliteData:
+    def __init__(self, data):
+        self.time_BME_data = data['time_BME']
+        self.time_ADXL_data = data['time_ADXL']
+        self.time_LIS_data = data['time_LIS']
+        self.humidity_data = data['humidity']
+        self.temp_data = data['temperature']
+        self.accelerometer_x_data = data['accelerometer_x']
+        self.accelerometer_y_data = data['accelerometer_y']
+        self.accelerometer_z_data = data['accelerometer_z']
+        self.magnetometer_x_data = data['magnetometer_x']
+        self.magnetometer_y_data = data['magnetometer_y']
+        self.magnetometer_z_data = data['magnetometer_z']
+
+satellite_data = SatelliteData(sh.satellites_data['luna_sat1'])
+
+
+# This function represents the main process
+def main_process():
+    app = pg.mkQApp("2D Graphs")
+    #mw = QtWidgets.QMainWindow()
+    #mw.resize(800,800)
+
+    win = pg.GraphicsLayoutWidget(show=True, title="2D Graphs Layout")
+    win.resize(1000,600)
+    win.setWindowTitle('LunaSat Graphs')
+
+    # Enable antialiasing for prettier plots
+    pg.setConfigOptions(antialias=True)
+
+    p1 = win.addPlot(title="Temperature vs Time")
+    c1 = p1.plot(pen='y', symbolPen='r')
+    
+    p2 = win.addPlot(title="Humidity vs Time")
+    c2 = p2.plot(pen='g', symbolPen='r')
+
+    win.nextRow()
+
+    p3 = win.addPlot(title="Accelerometer vs Time")
+    c3_x = p3.plot(pen='r', symbolPen='r')
+    c3_y = p3.plot(pen='g', symbolPen='r')
+    c3_z = p3.plot(pen='b', symbolPen='r')
+
+    p4 = win.addPlot(title="Magnetometer vs Time")
+    c4_x = p4.plot(pen='r', symbolPen='r')
+    c4_y = p4.plot(pen='g', symbolPen='r')
+    c4_z = p4.plot(pen='b', symbolPen='r')
+
+    def update():
+        sh.read_serial_data()
+
+        # Set data
+        # BME
+        y1 = satellite_data.time_BME_data
+        x1 = satellite_data.temp_data
+        y2 = y1
+        x2 = satellite_data.humidity_data
+        c1.setData(y1,x1)
+        c2.setData(y2,x2)
+        # ADXL
+        y3   = satellite_data.time_ADXL_data
+        x3_x = satellite_data.accelerometer_x_data
+        x3_y = satellite_data.accelerometer_y_data
+        x3_z = satellite_data.accelerometer_z_data
+        c3_x.setData(y3,x3_x)
+        c3_y.setData(y3,x3_y)
+        c3_z.setData(y3,x3_z)
+        # LIS
+        y4   = satellite_data.time_LIS_data
+        x4_x = satellite_data.magnetometer_x_data
+        x4_y = satellite_data.magnetometer_y_data
+        x4_z = satellite_data.magnetometer_z_data
+        c4_x.setData(y4,x4_x)
+        c4_y.setData(y4,x4_y)
+        c4_z.setData(y4,x4_z)
+
+        
+        
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(50)
+
+
     pg.exec()
+
+main_process()
+# def background_process():
+#     sh.read_serial_data()
+#     # sh.tester()
+
+# if __name__ == "__main__":
+#     # Create a multiprocessing.Event object to serve as a flag
+#     flag = multiprocessing.Event()
+
+#     # Create a process for the main process
+#     main_proc = multiprocessing.Process(target=main_process)
+
+#     # Start the main process
+#     main_proc.start()
+
+#     # Wait for a few seconds
+#     time.sleep(4)
+
+#     # Set the flag to signal the process to terminate
+#     flag.set()
+
+#     # Wait for the main process to finish
+#     main_proc.join()
+
+#     print("Both processes have finished")
+
---
+++ Plotter/serial_helper.py
@@ -0,0 +1,139 @@
+import serial
+
+# Serial config
+baud_rate = 115200  # Change this to match the baud rate of your device 115200
+serial_port = "/dev/tty.usbmodem14201"
+
+# Initialize the serial connection
+ser = serial.Serial(serial_port, baud_rate)
+
+
+# Initialize lists to store data
+lunasats = []
+lunasats_data = []
+time_data = []
+time_BME_data = []
+time_ADXL_data = []
+time_LIS_data = []
+temp_data = []
+humidity_data = []
+accelerometer_x_data = []
+accelerometer_y_data = []
+accelerometer_z_data = []
+magnetometer_x_data = []
+magnetometer_y_data = []
+magnetometer_z_data = []
+
+counter1 = 0
+counter2 = 0
+counter3 = 0
+
+satellites_data = {
+    'luna_sat1': {'time_BME': [], 'time_ADXL': [], 'time_LIS': [], 'humidity': [], 'temperature': [], 'accelerometer_x': [], 'accelerometer_y': [], 'accelerometer_z': [],
+                  'magnetometer_x': [], 'magnetometer_y': [], 'magnetometer_z': []},
+    'luna_sat2': {'time_BME': [], 'time_ADXL': [], 'time_LIS': [], 'humidity': [], 'temperature': [], 'accelerometer_x': [], 'accelerometer_y': [], 'accelerometer_z': [],
+                  'magnetometer_x': [], 'magnetometer_y': [], 'magnetometer_z': []},
+}
+
+# satellites_data['luna_sat1']['time_BME'].append(0)
+
+class LunaSat:
+    def __init__(self, value, x, y, z, color):
+        self.value = value
+        self.position = (x, y, z)
+        self.color = color
+
+def add_unique_lunasat(data_points):
+    global counter1
+    global counter2
+    global counter3
+    if int(data_points[1]) < 10:
+        number = int(data_points[1])
+        if number not in lunasats:
+            lunasats.append(number)
+            print("added lunasat")
+    
+    if int(data_points[1]) != 1:
+        counter1 += 1
+    elif int(data_points[1]) == 1:
+        counter1 = 0
+    if int(data_points[1]) != 2:
+        counter2 += 1
+    elif int(data_points[1]) == 2:
+        counter2 = 0
+    # if int(data_points[1]) != 3:
+    #     counter3 += 1
+    # elif int(data_points[1]) == 3:
+    #     counter3 = 0
+
+    if counter1 > 10:
+        lunasats.remove(1)
+    if counter2 > 10:
+        lunasats.remove(2)
+    # if counter3 > 10:
+    #     lunasats.remove(3)
+
+    # print("Lunasats: ", lunasats)
+    # print("Counter 1:", counter1, " Counter2: ", counter2)
+
+# Function to read serial data
+def read_serial_data():
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()  # Read a line and strip newline
+
+        # Extract the data points
+        data_points = line.split(',')  # Split the line by commas
+
+        print("Data points:", data_points)
+        # print("Sensor:", data_points[0])
+
+        # Parse sensor data
+        if len(data_points) >= 2:
+            try:
+                add_unique_lunasat(data_points)
+
+                sensor_name = data_points[0]
+                luna_sat_num = data_points[1]
+
+                if sensor_name == 'BME':
+                    time = int(data_points[2])
+                    temp = float(data_points[3])
+                    humidity = float(data_points[4])
+                    satellites_data['luna_sat'+luna_sat_num]['time_BME'].append(time)
+                    satellites_data['luna_sat'+luna_sat_num]['temperature'].append(temp)
+                    satellites_data['luna_sat'+luna_sat_num]['humidity'].append(humidity)
+                    return sensor_name
+                elif sensor_name == 'ADXL':
+                    time = int(data_points[2])
+                    accelerometer_x = float(data_points[3])
+                    accelerometer_y = float(data_points[4])
+                    accelerometer_z = float(data_points[5])
+                    satellites_data['luna_sat'+luna_sat_num]['time_ADXL'].append(time)
+                    satellites_data['luna_sat'+luna_sat_num]['accelerometer_x'].append(accelerometer_x)
+                    satellites_data['luna_sat'+luna_sat_num]['accelerometer_y'].append(accelerometer_y)
+                    satellites_data['luna_sat'+luna_sat_num]['accelerometer_z'].append(accelerometer_z)
+                    return sensor_name
+                elif sensor_name == 'LIS':
+                    time = int(data_points[2])
+                    magnetometer_x = float(data_points[3])
+                    magnetometer_y = float(data_points[4])
+                    magnetometer_z = float(data_points[5])
+                    satellites_data['luna_sat'+luna_sat_num]['time_LIS'].append(time)
+                    satellites_data['luna_sat'+luna_sat_num]['magnetometer_x'].append(magnetometer_x)
+                    satellites_data['luna_sat'+luna_sat_num]['magnetometer_y'].append(magnetometer_y)
+                    satellites_data['luna_sat'+luna_sat_num]['magnetometer_z'].append(magnetometer_z)
+                    return sensor_name
+                else:
+                    print("ERROR NO DATA FOUND")
+                    return None
+
+            except ValueError:
+                print("Data Empty")
+                return None
+        else:
+            return None
+    else:
+        return None
+    
+def tester():
+    print("test")
